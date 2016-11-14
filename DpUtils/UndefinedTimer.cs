cce9c08dd119beba24c8af196f864cfecb7612df
dprimenko <dprimenko@protonmail.com>
2016-11-14 13:40:21 +0100

    Added UndefinedTimer util

---
+++ DpUtils/UndefinedTimer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Timers;
+
+namespace DpUtils
+{
+	public class UndefinedTimer
+	{
+		const long MIN_INTERVAL = 1;
+		private long interval;
+		private long intervalTemp;
+		System.Timers.Timer timer;
+
+		public UndefinedTimer (long interval)
+		{
+			this.intervalTemp = Math.Abs (interval);
+			IntervalConfig ();
+			TimerConfig ();
+		}
+
+		abstract void OnElapsedTime(Object source, System.Timers.ElapsedEventArgs e);
+
+
+		private void IntervalConfig() {
+
+			if (interval < MIN_INTERVAL) {
+				this.interval = MIN_INTERVAL;
+			} 
+			else {
+				this.interval = intervalTemp;
+			}
+		}
+		private void TimerConfig() {
+
+			if (this.timer == null) {
+				this.timer = new System.Timers.Timer (interval);
+				this.timer.Elapsed += new ElapsedEventHandler (OnElapsedTime);
+			}
+		}
+
+		public sealed void Stop() {
+			this.timer.Stop ();
+		}
+
+		public sealed void Cancel() {
+			Stop ();
+			this.timer.Close ();
+		}
+
+		public sealed void Start() {
+			this.timer.Start();
+		}
+	}
+}
+
+
+
